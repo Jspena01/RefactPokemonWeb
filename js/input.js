f51b5d2bdd6cc1683e74749934655dbd24cbc465
Jspena01 <josesamuelrd@gmail.com>
2021-03-02 00:13:21 +0100

    Diseño del main character, comentario de los scripts y arreglo en el script de colisiones

--- js/input.js
+++ js/input.js
@@ -4,42 +4,39 @@ export default class inputHandler {
   constructor(characterP1) {
     //Detect if the character will collide with a border wall/floor
     this.limitRX = () => {
-      return (
-        characterP1.position.x + characterP1.speed >= characterP1.gameWidth
-      );
+      return characterP1.position.x + characterP1.size >= characterP1.gameWidth;
     };
     this.limitBY = () => {
       return (
-        characterP1.position.y + characterP1.speed >= characterP1.gameHeight
+        characterP1.position.y + characterP1.size >= characterP1.gameHeight
       );
     };
     this.limitLX = () => {
-      return characterP1.position.x - characterP1.speed <= 0;
+      return characterP1.position.x - characterP1.size <= 0;
     };
     this.limitUY = () => {
-      return characterP1.position.y - characterP1.speed <= 0;
+      return characterP1.position.y - characterP1.size <= 0;
     };
 
     document.addEventListener("keydown", (e) => {
       switch (e.keyCode) {
         case 87:
           characterP1.moveUp();
-          if (this.limitUY()) characterP1.position.y = 0;
+          if (this.limitUY()) characterP1.position.y = 0 + characterP1.size;
           break;
         case 83:
           characterP1.moveBottom();
           if (this.limitBY())
-            characterP1.position.y =
-              characterP1.gameHeight - characterP1.height;
+            characterP1.position.y = characterP1.gameHeight - characterP1.size;
           break;
         case 68:
           characterP1.moveRight();
           if (this.limitRX())
-            characterP1.position.x = characterP1.gameWidth - characterP1.width;
+            characterP1.position.x = characterP1.gameWidth - characterP1.size;
           break;
         case 65:
           characterP1.moveLeft();
-          if (this.limitLX()) characterP1.position.x = 0;
+          if (this.limitLX()) characterP1.position.x = 0 + characterP1.size;
           break;
       }
     });
--- js/paddles.js
+++ js/paddles.js
@@ -1,21 +1,82 @@
 export default class Paddle {
   constructor(gameWidth, gameHeight) {
-    this.width = 20;
-    this.height = 20;
+    this.size = 50;
     this.speed = 10;
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
 
     this.position = {
-      x: gameWidth / 2 - this.width / 2,
-      y: gameHeight / 2 - this.height / 2,
+      x: gameWidth / 2,
+      y: gameHeight / 2,
     };
     this.limitX = this.position.x + this.speed >= this.gameWidth + this.speed;
   }
 
   draw(context) {
-    context.fillStyle = "#f00";
-    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    //Dibuja una circunferencia roja y le elimina la mitad
+    context.beginPath();
+    context.fillStyle = "red";
+    context.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    //Dibuja la mitad de una circunferencia blanca
+    context.beginPath();
+    context.fillStyle = "white";
+    context.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 1);
+    context.fill();
+    context.stroke();
+
+    //Dibuja una linea en medio
+
+    context.beginPath();
+    context.fillStyle = "Black";
+    context.fillRect(
+      this.position.x - this.size,
+      this.position.y,
+      this.size * 2,
+      1.5
+    );
+
+    //Realiza las lineas de un plano cartesiano para verificar el centro
+    // context.beginPath();
+    // context.fillRect(400,0,1,this.gameHeight);
+    // context.fillRect(0,400,this.gameWidth,1);
+
+    //Dibuja una circunferencia con una circunferencia de la 6ta parte del tamaño del tamaño total del circulo y lo llena de blanco
+    context.beginPath();
+    context.fillStyle = "white";
+    context.arc(
+      this.position.x,
+      this.position.y,
+      this.size / 6,
+      0,
+      Math.PI * 2
+    );
+    context.fill();
+    //Dibuja una circunferencia con una circunferencia de la 6ta parte del tamaño del tamaño total del circulo y pinta el borde de negro
+
+    context.beginPath();
+    context.arc(
+      this.position.x,
+      this.position.y,
+      this.size / 6,
+      0,
+      Math.PI * 2
+    );
+    context.stroke();
+
+    //Repite el mismo circulo anterior, para aumentar la intensidad del color
+
+    context.beginPath();
+    context.arc(
+      this.position.x,
+      this.position.y,
+      this.size / 6,
+      0,
+      Math.PI * 2
+    );
+    context.stroke();
   }
   update(deltaTime) {
     if (!deltaTime) return;
